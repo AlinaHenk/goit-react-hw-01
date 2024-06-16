b15169e485eaec5e7cd92cafb4bab65b4f0d976e
AlinaHenk <160937471+AlinaHenk@users.noreply.github.com>
2024-06-16 21:43:36 +0300

    first task is ready

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import Profile from "../Profile/Profile";
+import userData from "../userData.json";
+import friends from "../friends.json";
 
 export default function App() {
   return (
-    <div>
-      <p>Hello</p>
-      <Profile />
-    </div>
+    <>
+      <Profile userData={userData} />
+      <FriendList friends={friends} />
+    </>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,34 @@
-export default function Profile() {
+import css from "./Profile.module.css";
+
+export default function Profile({
+  userData: { username, tag, location, avatar, stats },
+}) {
   return (
-    <div>
-       {" "}
-      <div>
-           {" "}
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-        />
-            <p>Petra Marica</p>    <p>@pmarica</p>    <p>Salvador, Brasil</p> {" "}
+    <div className={css.container}>
+      <div className={css.profile}>
+        <div className={css.mainInfo}>
+          <div className={css.avatarWrapper}>
+            <img className={css.avatar} src={avatar} alt="User avatar" />
+          </div>
+          <p className={css.name}>{username}</p>
+          <p className={css.tag}>{tag}</p>
+          <p className={css.location}>{location}</p>
+        </div>
+        <ul className={css.bottomLine}>
+          <li className={css.bottomItem}>
+            <span>Followers</span>
+            <span className={css.boldItem}>{stats.followers}</span>
+          </li>
+          <li className={css.bottomItem}>
+            <span>Views</span>
+            <span className={css.boldItem}>{stats.views}</span>
+          </li>
+          <li className={css.bottomItem}>
+            <span>Likes</span>
+            <span className={css.boldItem}>{stats.likes}</span>
+          </li>
+        </ul>
       </div>
-       {" "}
-      <ul>
-           {" "}
-        <li>
-                <span>Followers</span>      <span>1000</span>   {" "}
-        </li>
-           {" "}
-        <li>
-                <span>Views</span>      <span>2000</span>   {" "}
-        </li>
-           {" "}
-        <li>
-                <span>Likes</span>      <span>3000</span>   {" "}
-        </li>
-         {" "}
-      </ul>
     </div>
   );
 }
