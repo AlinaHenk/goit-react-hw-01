d62d38223f9db37c9c04e0e1a3cbe4de09764d13
AlinaHenk <160937471+AlinaHenk@users.noreply.github.com>
2024-06-16 23:17:57 +0300

    task two is ready

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,10 +1,14 @@
-import 
-export default function FriendList() {
+import FriendListItem from "../FriendListItem/FriendListItem";
+import css from "./FriendList.module.css";
+
+export default function FriendList({ friends }) {
   return (
-    <ul>
-      <li>
-        <FriendListItem />
-      </li>
+    <ul className={css.friendList}>
+      {friends.map((item) => (
+        <li key={item.id}>
+          <FriendListItem friend={item} />
+        </li>
+      ))}
     </ul>
   );
 }
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,13 @@
-export default function FriendListItem() {
+import css from "./FriendListItem.module.css";
+
+export default function FriendListItem({ friend: { avatar, name, isOnline } }) {
   return (
-    <div>
-      <img src="" alt="Avatar" width="48" />
-      <p>Friend name</p>
-      <p>Friend status</p>
+    <div className={css.friendCard}>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p className={css.name}>{name}</p>
+      <p className={isOnline ? css.online : css.offline}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
     </div>
   );
 }
